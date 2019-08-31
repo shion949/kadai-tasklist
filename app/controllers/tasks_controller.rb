1938ce08de86a5484fc2e2020c32e70ef8efe6f9
yuki.furuno <9837ghaz@jcom.zaq.ne.jp>
2019-08-31 15:31:36 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,26 @@
+class TasksController < ApplicationController
+  def index
+      @tasks = Task.all
+  end
+
+  def show
+      @tasks = Task.find(params[:id])
+  end
+
+  def new
+      @tasks = Task.new
+  end
+
+  def create
+  end
+
+  def edit
+      @tasks = Task.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
