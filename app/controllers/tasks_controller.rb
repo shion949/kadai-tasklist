910218a1c046653b2a5b77590596414ba5098d30
yuki.furuno <9837ghaz@jcom.zaq.ne.jp>
2019-09-02 12:20:07 +0000

    CRUD messages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,15 @@ class TasksController < ApplicationController
   end
 
   def create
+    @tasks = Task.new(task_params)
+
+    if @tasks.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @tasks
+    else
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+    end
   end
 
   def edit
@@ -19,8 +28,29 @@ class TasksController < ApplicationController
   end
 
   def update
+    @tasks = Task.find(params[:id])
+
+    if @tasks.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @tasks
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
   end
 
   def destroy
+    @tasks = Task.find(params[:id])
+    @tasks.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
   end
+  
+  private
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
+  
 end
