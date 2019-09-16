b28c7357c2bd1b78a619fb355780a60b9816ed1d
yuki.furuno <9837ghaz@jcom.zaq.ne.jp>
2019-09-16 07:50:37 +0000

    kadai improve3

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
+  #get 'sessions/create'
+  #get 'sessions/destroy'
+  #get 'users/index'
+  #get 'users/show'
+  #get 'users/new'
+  #get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
 
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
   
-  resources :microposts, only: [:create, :destroy]
+  #resources :microposts, only: [:create, :destroy]
 end
